de1704b1824f166cc620b74ff2887ae834c6f522
yejis2 <yejif15@gmail.com>
2023-08-01 02:35:02 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/test-for-instruqt/network/azurerm"
-  version = "5.3.0"
+  version = "3.5.0"
   resource_group_name = "azurerm_resource_group.myresourcegroup.name"
 }
