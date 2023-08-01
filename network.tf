8afdea463b9c1d0ec62d61ae76bd508cbf047bac
yejis2 <yejif15@gmail.com>
2023-08-01 02:28:01 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,20 +1,5 @@
 module "network" {
-  # source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  # address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  # subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  # subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  # subnet_service_endpoints = {
-  #   "subnet1" : ["Microsoft.Sql"],
-  #   "subnet2" : ["Microsoft.Sql"],
-  #   "subnet3" : ["Microsoft.Sql"]
-  # }
-  use_for_each = true
-  # tags = {
-  #   environment = "dev"
-  #   costcenter  = "it"
-  # }
-
-  # depends_on = [azurerm_resource_group.example]
+  source  = "app.terraform.io/test-for-instruqt/network/azurerm"
+  version = "5.3.0"
+  # insert required variables here
 }
